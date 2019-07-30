c3f1e19415bb5cdcf67c92d7334a6ff38dd6c068
afreen shaik <sk.afreen12@gmail.com>
2019-07-30 08:00:40 +0530

    spring task4 using annotations

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -1,20 +1,20 @@
 package com.stackroute.domain;
 public class Actor {
-    private String actorName;
-    private String gender;
-    private int age;
 
-    public Actor(String actorName, String gender, int age) {
-        this.actorName = actorName;
+    String name;
+    String gender;
+    int age;
+
+    public Actor(String name, String gender, int age) {
+        this.name = name;
         this.gender = gender;
         this.age = age;
     }
 
-
     @Override
     public String toString() {
         return "Actor{" +
-                "actorName='" + actorName + '\'' +
+                "name='" + name + '\'' +
                 ", gender='" + gender + '\'' +
                 ", age=" + age +
                 '}';
--- src/main/java/com/stackroute/domain/AppConfig.java
+++ src/main/java/com/stackroute/domain/AppConfig.java
@@ -6,20 +6,52 @@ import org.springframework.context.annotation.Scope;
 
 
 public class AppConfig {
+
+    Actor actor1,actor2,actor3;
+
     @Bean(name = "actor1")
     public Actor getActor1(){
 
-        Actor actor1 = new Actor("Shakti","male",30);
+        actor1 = new Actor("Arjun","male",30);
+        return actor1;
+    }
+
+    @Bean(name = "actor2")
+    public Actor getActor2(){
+
+        actor2 = new Actor("Vijay","male",37);
+        return actor1;
+    }
+
+    @Bean(name = "actor3")
+    public Actor getActor3(){
+
+        actor3 = new Actor("Rani","female",32);
         return actor1;
     }
 
 
     @Bean(name = "movie1")
-    @Scope("prototype")
     public Movie getMovie1(){
         Movie movie1 = new Movie();
 
+        movie1.setActor1(actor1);
+        movie1.setActor2(actor2);
+        movie1.setActor3(actor3);
+
         return movie1;
     }
+
+    @Bean(name = "movie2")
+    public Movie getMovie2(){
+        Movie movie2 = new Movie();
+
+        movie2.setActor1(actor1);
+        movie2.setActor2(actor2);
+        movie2.setActor3(actor3);
+
+        return movie2;
+    }
+
 
 }
